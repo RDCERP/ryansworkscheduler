61732e3c42e62a1d414f2b2299acefc881ce82ba
RDCERP <109607056+RDCERP@users.noreply.github.com>
2022-09-14 10:52:38 -0500

    made 9am time block

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,7 @@
 // When you view the time blocks for that day, each time block is color coded to indicate whether it is in the past, present, or future
 // When you click on the save button for that time slot, the text for that event is saved in local storage
 // When the page is refreshed, the saved events persist
+
+
+var: currentDay = moment().format('dddd, MMMM Do YYYY');
+var: currentHour = moment().format('H');
