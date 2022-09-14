f60393a3a9b7cd227446551112d4dd57c676d05e
RDCERP <109607056+RDCERP@users.noreply.github.com>
2022-09-14 10:26:42 -0500

    initial set-up

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// The current day is displayed at the top of the calendar when the planner is opened using moment.js
+// I am presented with timeblocks for standard business hours
+// When you view the time blocks for that day, each time block is color coded to indicate whether it is in the past, present, or future
+// When you click on the save button for that time slot, the text for that event is saved in local storage
+// When the page is refreshed, the saved events persist
