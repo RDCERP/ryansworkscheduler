ffa69b813a11f8bc46350bf55fb4c9d7ee6b95c2
RDCERP <109607056+RDCERP@users.noreply.github.com>
2022-09-14 14:16:54 -0500

    fixed bugs added buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,64 @@
 // When you click on the save button for that time slot, the text for that event is saved in local storage
 // When the page is refreshed, the saved events persist
 
+let timeBlocks = $(".time-block");      // time blocks
+let saveBtn = $(".saveBtn");            // save buttons
+let textArea = $(".description");       // text areas
+let time = $(".hour");                  // hours
+let timeArray = [];                     // array of hours
+let textArray = [];                     // array of text areas
+let timeBlockArray = [];                // array of time blocks
+let timeBlockHourArray = [];            // array of time block hours
 
-var currentDay = moment().format('dddd, MMMM Do YYYY');     // current day
-var currentHour = moment().format('H');                     // current hour
-var currentMinute = moment().format('mm');                  // current minute
+$(document).ready(function () {
 
-$("#currentDay").text(currentDay + " " + currentHour + ":" + currentMinute);        // display current day
+    var now = moment();                         // current time
+    $("#currentDay").text(now);                 // display current time   
+        setInterval(function () {               // update time every second
+        now = moment();
+        $("#currentDay").text(now);
+        } , 1000);
+
+    $(".saveBtn").on("click", function () {                   // save button click event
+        let text = $(this).siblings(".description").val();    // get text area value
+        let time = $(this).parent().attr("id");               // get time block id
+        localStorage.setItem(text, JSON.stringify(text));  // save text and time to local storage            
+        // save text area value to local storage
+        alert("Saved!");                                      // alert user that text area value was saved
+    });
+    // get time blocks
+
+        // loop through time blocks
+        for (let i = 0; i < timeBlocks.length; i++) {
+    
+            // push time blocks to array
+            timeBlockArray.push(timeBlocks[i]);
+    
+            // push time block hours to array
+            timeBlockHour.push(timeBlockArray[i].children[0].innerText);
+    
+            // push time block hours to array
+            timeBlockHourArray.push(timeBlockHour[i].slice(0, 2));
+    
+            // push text areas to array
+            textArray.push(textArea[i]);
+    
+            // push save buttons to array
+            saveArray.push(saveBtn[i]);
+    
+            // push hours to array
+            timeArray.push(time[i]);
+    
+            // push hours to array
+            timeArray.push(time[i]);
+            // console.log(timeArray);
+       } });
+
+
+
+    
+
+
+
+
+
